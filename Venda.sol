20484d48cd98bc3376896e2bd6593c8bab17299a
Bruno Fantineli <bruno.fantinelli0@gmail.com>
2019-05-07 10:58:59 -0300

    Add files via upload

--- Venda.sol
+++ Venda.sol
@@ -7,22 +7,26 @@ contract Venda{
   enum State {Criado, Travado, Inativo}
   State public state;
 
+  //Criar venda com um determinado valor
   function Venda() payable{
     vendedor = msg.sender
     valor = msg.value;
   }
 
+  //Modificadores para controlar acesso nas funções
   modifier condition(bool _condition){
     require(_condition);
     _;
   }
 
   modifier apenasComprador(){
+    //Requer que quem chamou o contrato seja o comprador
     require(msg.sender == comprador);
     _;
   }
 
   modifier apenasVendedor(){
+    //Requer que quem chamou o contrato seja o vendedor
     require(msg.sender == vendedor);
     _;
   }
@@ -32,16 +36,19 @@ contract Venda{
     _;
   }
 
+  //Eventos
   event Abortar();
   event CompraConfirmada();
   event ItemRecebido();
 
+  //Finalizar uma compra, qualquer um pode chamar essa função
   function confimarCompra() noState(State.Criado) condition(msg.value == valor) payable {
     CompraConfirmada();
     comprador = msg.sender;
     state = State.Travado;
   }
 
+  //Apenas quem comprou o item na função acima pode acessar essa função
   function confimarRecebido() apenasComprador noState(State.Travado) {
     ItemRecebido();
     state = State.Inativo;
@@ -50,6 +57,7 @@ contract Venda{
     vendedor.transfer(this.balance);
   }
 
+  //Apenas o vendedor pode acessar a função, para cancelar a venda de algum item
   function abortar() apenasVendedor noState(State.Criado){
     Abortar();
     state = State.Inativo;
